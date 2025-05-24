de74dad30d2994f355565b9dc9b68657f290e99f
Apple123-pm <pmmpiimmm@gmail.com>
2025-05-25 01:53:41 +0700

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const mqtt = require("mqtt");
 
